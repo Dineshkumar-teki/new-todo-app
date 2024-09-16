69fed41b814e8c5d8a1e52ac5631c553b783ac86
Dineshkumar-teki <dineshkumarteki497@gmail.com>
2024-09-16 12:59:23 +0530

    improve

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import todoRoutes from "./routes/todoRoutes.js";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 
@@ -12,11 +13,17 @@ app.use(cors());
 
 dotenv.config();
 
+const __dirname = path.resolve();
+
 app.get("/", (request, response) => {
   response.status(200).send("Server is live");
 });
 
 app.use("/todos", todoRoutes);
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 
 mongoose
   .connect(process.env.MONGO_DB_URL)
